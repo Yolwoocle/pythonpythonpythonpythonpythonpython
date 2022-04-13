f7dd4629016fc937aaa539057eccffe805ce270b
Yolwoocle <leo.bernard2015@hotmail.com>
2022-04-13 08:48:34 +0200

    PythonPythonPythonPythonPythonPythonPythonPythonPy

---
+++ a (448th copy).py
@@ -0,0 +1,17 @@
+def hello():
+	print('Hello, world!')
+
+class HelloWorld:
+	def __init__(self):
+		print('Hello, world!')
+
+# Prints the best message in the world
+def best_message():
+	print('Hello, world!')
+	print('Hello, world!')
+	print('Hello, world!')
+	print('Hello, world!')
+
+def ratatouille(best_movie):
+	for i in range(5):
+		print('Hello, world!')
